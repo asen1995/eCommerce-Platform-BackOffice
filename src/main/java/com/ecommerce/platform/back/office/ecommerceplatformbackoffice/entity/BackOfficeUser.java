08d4e82ca51d0779cbbdab3d73e23c6898d0e282
Asen Nikolaev <asen.nikolaev95@abv.bg>
2023-03-02 13:06:53 +0200

    configure users with ORDER_MANAGER to listen for notifications for creating new orders

--- src/main/java/com/ecommerce/platform/back/office/ecommerceplatformbackoffice/entity/BackOfficeUser.java
+++ src/main/java/com/ecommerce/platform/back/office/ecommerceplatformbackoffice/entity/BackOfficeUser.java
@@ -3,8 +3,16 @@ package com.ecommerce.platform.back.office.ecommerceplatformbackoffice.entity;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.ToString;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.messaging.converter.StringMessageConverter;
+import org.springframework.messaging.simp.stomp.*;
+import org.springframework.web.socket.client.WebSocketClient;
+import org.springframework.web.socket.client.standard.StandardWebSocketClient;
+import org.springframework.web.socket.messaging.WebSocketStompClient;
 
 import javax.persistence.*;
+import java.lang.reflect.Type;
 import java.util.List;
 
 @Entity
@@ -17,16 +25,49 @@ import java.util.List;
 @EqualsAndHashCode(exclude = {"roles"})
 public class BackOfficeUser {
 
-        @Id
-        private Integer id;
+    private static final Logger logger = LogManager.getLogger(BackOfficeUser.class);
 
-        @Column(name = "bo_username")
-        private String username;
+    @Id
+    private Integer id;
 
-        @Column(name = "bo_password")
-        private String password;
+    @Column(name = "bo_username")
+    private String username;
 
-        @ManyToMany(fetch = FetchType.EAGER)
-        private List<Role> roles;
+    @Column(name = "bo_password")
+    private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    private List<Role> roles;
+
+
+    public void listenForNewOrderTopicMessages(String topic, String url) {
+
+        WebSocketClient webSocketClient = new StandardWebSocketClient();
+        WebSocketStompClient stompClient = new WebSocketStompClient(webSocketClient);
+        stompClient.setMessageConverter(new StringMessageConverter());
+
+        StompSessionHandler sessionHandler = new StompSessionHandlerAdapter() {
+            @Override
+            public void afterConnected(StompSession session, StompHeaders connectedHeaders) {
+                session.subscribe(topic, new StompFrameHandler() {
+                    @Override
+                    public Type getPayloadType(StompHeaders headers) {
+                        return String.class;
+                    }
+
+                    @Override
+                    public void handleFrame(StompHeaders headers, Object payload) {
+                        logger.info("Hey " + username + ", a new order has been placed! : " + payload);
+                    }
+                });
+            }
+        };
+
+        try {
+            stompClient.connect(url, sessionHandler).get();
+        } catch (Exception e) {
+            logger.error("Error connecting to topic " + topic);
+        }
+    }
 
 }
--- src/main/java/com/ecommerce/platform/back/office/ecommerceplatformbackoffice/security/LoggedInUsers.java
+++ src/main/java/com/ecommerce/platform/back/office/ecommerceplatformbackoffice/security/LoggedInUsers.java
@@ -1,8 +1,10 @@
 package com.ecommerce.platform.back.office.ecommerceplatformbackoffice.security;
 
 import com.ecommerce.platform.back.office.ecommerceplatformbackoffice.entity.BackOfficeUser;
+import com.ecommerce.platform.back.office.ecommerceplatformbackoffice.enums.RoleEnum;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PreDestroy;
@@ -14,6 +16,10 @@ public class LoggedInUsers {
 
     private static final Logger logger = LogManager.getLogger(LoggedInUsers.class);
 
+
+    @Value("${back-office-websocket-service.url}")
+    private String backOfficeWebSocketUrl;
+
     private final Map<String, BackOfficeUser> users = new ConcurrentHashMap<>();
 
     public void loginUser(BackOfficeUser backOfficeUser) {
@@ -22,6 +28,11 @@ public class LoggedInUsers {
         users.put(backOfficeUser.getUsername(), backOfficeUser);
         logger.info("User logged in: " + backOfficeUser.getUsername());
 
+        if (backOfficeUser.getRoles().stream().anyMatch(role -> role.getRoleName() == RoleEnum.ORDER_MANAGER)) {
+            logger.info("User {} is an order manager, subscribing to order topic", backOfficeUser.getUsername());
+            backOfficeUser.listenForNewOrderTopicMessages("/topic/order", backOfficeWebSocketUrl);
+            logger.info("User {} subscribed to order topic successfully", backOfficeUser.getUsername());
+        }
     }
 
     public void logoutUser(BackOfficeUser user) {
